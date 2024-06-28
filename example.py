2eafb8a0f0812897f6e83fc0530694148237558d
Adam Aaronson <adamaaronson@gmail.com>
2024-06-28 12:11:08 -0400

    Add numpydoc

--- example.py
+++ example.py
@@ -1,3 +1,13 @@
-def my_function(a):
-    """My function."""
+"""Example file."""
+
+
+def my_function(a: int):
+    """
+    My function.
+
+    Parameters
+    ----------
+    a : int
+        The parameter.
+    """
     pass
